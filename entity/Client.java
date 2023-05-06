aa95ff666016953fadd231bb968d54b2d33afd7a
YaiReich <117544089+YaiReich@users.noreply.github.com>
2023-05-06 23:46:32 +0300

    Update Client.java

--- entity/Client.java
+++ entity/Client.java
@@ -16,7 +16,11 @@ import javax.persistence.Id;
 import javax.persistence.OneToOne;
 import javax.persistence.Version;
 
-
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter
